1456bdb66aa88935679436f3b6e279e6a29b01b8
Shuvradeb Saha <bsse0815@iit.du.ac.bd>
2019-03-12 13:04:09 +0600

    c3

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/weather-app-0815'));
+app.use(express.static(__dirname + '/dist/weather-app'));
 
 app.get('/*', function (req, res) {
-
-  res.sendFile(path.join(__dirname + '/dist/weather-app-0815/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/weather-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
